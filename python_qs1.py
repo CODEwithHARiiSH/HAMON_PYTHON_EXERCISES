9a9dda0586368920799b6d6d9f61ee237fad594c
CODEwithHARiiSH <harishthampyhat@gmail.com>
2023-10-30 15:40:06 +0530

    Done question two. I have completed question two and this commit is for adding this.

--- python_qs1.py
+++ python_qs1.py
@@ -25,4 +25,21 @@ def fizzbizz(n):
 
 
 
-fizzbizz(" ") 
+fizzbizz(" ")
+
+
+# QUESTION2. Write a python function called palindrome that receives a string s as an
+#    argument. It will return True if s is a palindrome and False if
+#    not. A palindrome is a word that will read the same from left
+#    to right and right to left. e.g. "dad", "abba"
+
+
+def palindrome(s):
+    s=input("enter a word  :  ")
+    if s == s[::-1]:
+        print("True")
+    else:
+        print("False")
+
+
+palindrome('')
