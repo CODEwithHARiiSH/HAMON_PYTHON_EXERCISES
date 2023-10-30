8ad68ef346ed35dbeffce2ad6da4eb99b5b3d803
CODEwithHARiiSH <harishthampyhat@gmail.com>
2023-10-30 15:44:48 +0530

    Done question three. i have done question three and iam uploading that

--- python_qs1.py
+++ python_qs1.py
@@ -40,6 +40,38 @@ def palindrome(s):
         print("True")
     else:
         print("False")
-
-
+        
 palindrome('')
+
+
+
+
+
+
+
+    # QUESTION3. Write a python function called panagram that receives a string s as an
+    #    argument. It will return True if s is a panagram and False if
+    #    not. A panagram is a sentence that contains all letters of the
+    #    alphabet. e.g. "the quick brown fox jumps over the lazy dog"
+
+
+def panagram(s):
+    s=input("enter your sentence : ")
+    ansr=" "
+    ref_string = 'abcdefghijklmnopqrstuvwxyz '
+    for i in ref_string:
+        if i in s:
+            ansr=True
+        else:
+            ansr=False
+            break
+    return ansr
+  
+myfunction=panagram(" ")
+print(myfunction)
+
+
+
+
+
+
